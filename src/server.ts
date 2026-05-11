563bae0e09b53fdde33dee83bd56db0d0146a0b6
bansalbhunesh <bhuneshbansal20039888@gmail.com>
2026-05-11 10:35:43 +0530

    fix: resolve vercel build error by simplifying server entry

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
 import { createStartHandler, defaultStreamHandler } from "@tanstack/react-start/server";
-import { getStartManifest as getRouterManifest } from "@tanstack/start-server-core/router-manifest";
-import { createRouter } from "./router";
 
-export default createStartHandler({
-  createRouter,
-  getRouterManifest,
-  handler: defaultStreamHandler,
-});
+export default createStartHandler(defaultStreamHandler);
